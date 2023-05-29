8c27cd94cf94c202db1814cb45bd4f26a429d637
Ryan Stainsby <rstainsbydevelopment@gmail.com>
2023-05-29 12:00:03 +1000

    add buttonset story

---
+++ src/stories/ButtonSet.stories.svelte
@@ -0,0 +1,32 @@
+<script lang='ts'>
+    import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+    import { ButtonSet, Button } from 'carbon-components-svelte';
+    import 'carbon-components-svelte/css/all.css';
+</script>
+
+<Meta 
+    title="Carbon Design/ButtonSet" 
+    component={ButtonSet} 
+    argTypes={{
+        stacked: {
+            control: 'boolean',
+            description: 'Set to true to stack buttons vertically',
+            table: {
+                type: { summary: null },
+                defaultValue: { summary: false }
+            }
+        }
+    }}
+/>
+
+<Template let:args>
+    <ButtonSet {...args}>
+        <Button>Button 1</Button>
+        <Button kind={'secondary'}>Button 2</Button>
+        <Button kind={'tertiary'}>Button 3</Button>
+    </ButtonSet>
+</Template>
+
+<Story name="Horizontal" args={{ stacked: false }}></Story>
+
+<Story name="Vertical" args={{ stacked: true }}></Story>
